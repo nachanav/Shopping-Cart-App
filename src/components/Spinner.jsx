623e934bbbcbe8975e043b3810d2c914539680dd
Akash Nachan <90974084+nachanav@users.noreply.github.com>
2023-06-06 18:07:46 +0530

    Add files via upload

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Spinner.css"
+
+const Spinner = () => {
+  return (
+    <div className="items-center justify-center absolute">
+      <div className='spinner'></div>
+    </div>
+  )
+}
+
+export default Spinner
